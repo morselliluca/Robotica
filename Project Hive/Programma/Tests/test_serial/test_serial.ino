131ac00ef65344505b15094fa8ee921abb7d4d61
morselliluca <79109238+morselliluca@users.noreply.github.com>
2022-02-10 21:55:58 +0200

    Create test_serial.ino

---
+++ Project Hive/Programma/Tests/test_serial/test_serial.ino
@@ -0,0 +1,20 @@
+void setup() {
+  Serial.begin(9600);
+  }
+
+String input;
+String speedL;
+String speedR;
+String cubi;
+
+void loop() {
+
+    input = Serial.readStringUntil('x');
+        speedL = String(input[2]) + String(input[3]) + String(input[4]);
+        speedR = String(input[8]) + String(input[9]) + String(input[10]);
+        cubi = String(input[12]);
+        Serial.println(speedL);
+        Serial.println(speedR);
+        Serial.println(cubi);
+        Serial.println(input);
+}

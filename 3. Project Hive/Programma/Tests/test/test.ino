3d5795969731b89419f845c5e1468c6167521279
morselliluca <79109238+morselliluca@users.noreply.github.com>
2022-04-16 12:19:45 +0200

    Update test.ino

--- 3. Project Hive/Programma/Tests/test/test.ino
+++ 3. Project Hive/Programma/Tests/test/test.ino
@@ -3,7 +3,6 @@
 Adafruit_MLX90614 mlx;
 
 #define IR1 0x5C //indirizzo ir davanti
-#define IR2 0x5A //indirizzo ir sinistra
 #define IR3 0x5B //indirizzo ir destra
 #define reflection1A A10 //riflesso analogico 1
 #define reflection2A A11 //riflesso analogico 2
@@ -17,11 +16,6 @@ void loop() {
   mlx.AddrSet(IR1);
   Serial.println("calore");
   Serial.print("Ambient = "); Serial.print(mlx.readAmbientTempC()); 
-  Serial.print("*C\tObject = "); Serial.print(mlx.readObjectTempC()); Serial.println("*C");
-  Serial.print("Ambient = "); Serial.print(mlx.readAmbientTempF()); 
-  Serial.print("*F\tObject = "); Serial.print(mlx.readObjectTempF()); Serial.println("*F");
-    mlx.AddrSet(IR2);
-    Serial.println("calore");Serial.print("Ambient = "); Serial.print(mlx.readAmbientTempC()); 
   Serial.print("*C\tObject = "); Serial.print(mlx.readObjectTempC()); Serial.println("*C");
   Serial.print("Ambient = "); Serial.print(mlx.readAmbientTempF()); 
   Serial.print("*F\tObject = "); Serial.print(mlx.readObjectTempF()); Serial.println("*F");

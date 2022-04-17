de6a61d6bb424d201c7ae59078454a4154efd9aa
LucaBarban <45258881+LucaBarban@users.noreply.github.com>
2022-04-17 10:25:44 +0200

    Update prova_v20.ino

--- 3. Project Hive/Programma/Teensy/prova_v20/prova_v20.ino
+++ 3. Project Hive/Programma/Teensy/prova_v20/prova_v20.ino
@@ -168,8 +168,8 @@ ros::Publisher hot("calore", & str_msg);
 ros::Publisher str("partito", & str_msg);
 
 ros::Subscriber < std_msgs::String > sub2("cubi", & cubimsg);
-ros::Subscriber < std_msgs::String > sub3("ready", & redygo);
-ros::Subscriber < geometry_msgs::Twist > sub1("cmd_vel", velCallback); //create a subscriber for ROS cmd_vel topic
+ros::Subscriber < std_msgs::String > sub3("pronto", & redygo);
+ros::Subscriber < geometry_msgs::Twist > sub1("cmd_vel", & velCallback); //create a subscriber for ROS cmd_vel topic
 
 File dataFile;
 

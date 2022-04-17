93fb10244b56bb7c61d53556daa0b383cb8d7f60
morselliluca <79109238+morselliluca@users.noreply.github.com>
2022-04-17 10:47:44 +0200

    Update prova_v20.ino

--- 3. Project Hive/Programma/Teensy/prova_v20/prova_v20.ino
+++ 3. Project Hive/Programma/Teensy/prova_v20/prova_v20.ino
@@ -13,9 +13,9 @@
 ros::NodeHandle nh;
 std_msgs::String str_msg;
 
-/*ros::Publisher blk("nero", & str_msg);
+ros::Publisher blk("nero", & str_msg);
 ros::Publisher hot("calore", & str_msg);
-ros::Publisher str("partito", & str_msg);*/
+ros::Publisher str("partito", & str_msg);
 
 #define outputA1 29
 #define outputB1 30
@@ -167,25 +167,9 @@ void redygo(const std_msgs::String & msg) {
 
 }
 
-<<<<<<< HEAD
 ros::Subscriber < std_msgs::String > subcubi("cubi", & cubimsg);
 ros::Subscriber < std_msgs::String > subgo("ready", & redygo);
 ros::Subscriber < geometry_msgs::Twist > subvel("cmd_vel", velCallback); //create a subscriber for ROS cmd_vel topic
-=======
-//<<<<<<< HEAD
-ros::Subscriber < std_msgs::String > sub2("cubetti", & cubimsg);
-ros::Subscriber < std_msgs::String > sub3("partenza", & redygo);
-ros::Subscriber < geometry_msgs::Twist > sub1("cmd_vel", velCallback); //create a subscriber for ROS cmd_vel topic
-/*=======*/
-ros::Publisher blk("nero", & str_msg);
-ros::Publisher hot("calore", & str_msg);
-ros::Publisher str("partito", & str_msg);
-
-/*ros::Subscriber < std_msgs::String > sub2("cubi", & cubimsg);
-ros::Subscriber < std_msgs::String > sub3("pronto", & redygo);
-ros::Subscriber < geometry_msgs::Twist > sub1("cmd_vel", & velCallback); //create a subscriber for ROS cmd_vel topic
->>>>>>> de6a61d6bb424d201c7ae59078454a4154efd9aa*/
->>>>>>> 1b53f749741c286b74f0d74d6f889a5c61a784b9
 
 File dataFile;
 
@@ -454,6 +438,7 @@ void checkNero() {
 }
 
 void checkCalore() {
+  /*
   char temp[3] = {
     '0',
     '0',
@@ -471,7 +456,7 @@ void checkCalore() {
   if (5 < mlx.readObjectTempC() - mlx.readAmbientTempC()) {
     temp[2] = "R";
   }
-  calore = String(temp);
+  calore = String(temp);*/
 
 }
 
